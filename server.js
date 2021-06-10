c58e9cdf74bdff0b567e08d68232908ea0b7aa47
Dav-Sho <shodunkedavid39@gmail.com>
2021-06-10 16:17:55 -0700

    connect server to db & handle error

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
+// Load env var
 dotenv.config({path: './config/config.env'});
+
+// load DB
+connectDB();
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port: ${process.env.PORT}`
       .yellow.bold
   );
+});
+
+// handle unhandleRejection
+process.on('unhandledRejection', (err, promise) => {
+  console.error(err.message);
+  server.close(() => process.exit(1));
 });
