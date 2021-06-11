6426185a1fbf67a0d754d8c0cc19df652d70602b
Dav-Sho <shodunkedavid39@gmail.com>
2021-06-10 19:25:34 -0700

    User API Routes & Jwt Authentication

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,81 @@
 const express = require('express');
+const {check, validationResult} = require('express-validator');
+const User = require('../../models/User');
+const gravater = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 
 //@route   Get/api/users
-//@desc    user routes
+//@desc    Register user
 //@access  public
-router.get('/', (req, res) => {
-  res.send('User route');
-});
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Enter a valid email').isEmail(),
+    check(
+      'password',
+      'Password must have atlest min of 6 characters'
+    ).isLength({min: 6}),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({errors: errors.array()});
+    }
+
+    const {name, email, password} = req.body;
+
+    try {
+      let user = await User.findOne({email});
+
+      if (user) {
+        return res.status(400).json([{msg: 'User already eist'}]);
+      }
+
+      const avater = gravater.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm',
+      });
+
+      user = new User({
+        name,
+        email,
+        password,
+        avater,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      //   hash password
+      user.password = await bcrypt.hash(password, salt);
+
+      // save user
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        process.env.jwtSecrete,
+        {expiresIn: 360000},
+        (err, token) => {
+          if (err) throw err;
+          res.json({token});
+        }
+      );
+    } catch (err) {
+      console.error(err.msg);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -10,10 +10,15 @@ dotenv.config({path: './config/config.env'});
 connectDB();
 
 const app = express();
+
+// Init Middleware
+app.use(express.json({extended: false}));
+
+// Mount Route
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/post', require('./routes/api/posts'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
